c92342c9a78f78d125784f3cc9c1b4d292508311
rumenski11 <143481439+rumenski11@users.noreply.github.com>
2024-07-08 09:33:21 +0300

    Initial commit

---
+++ Software/DHT22 - Digital Thermometer and Humidimeter/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Adafruit_sensor.h>
+#include <DHT_U.h>
+#include <DHT.h>
+
+#define DHT22_PIN 13
+
+DHT dht22(DHT22_PIN, DHT22);
+
+void setup() {
+  Serial.begin(9600);
+  dht22.begin(); // initialize the DHT22 sensor
+}
+
+void loop() {
+  // wait a few seconds between measurements.
+  delay(2000);
+
+  // read humidity
+  float humi  = dht22.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht22.readTemperature();
+  // read temperature as Fahrenheit
+  float tempF = dht22.readTemperature(true);
+
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT22 sensor!");
+  } else {
+    Serial.print("DHT22# Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  "); 
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C ~ ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+}
